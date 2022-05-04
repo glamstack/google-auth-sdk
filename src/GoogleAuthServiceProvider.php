90718d10ecc4680983c0d6a6188c42402f50ee60
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-04 14:45:38 -0400

    Update src/GoogleAuthServiceProvider.php to use Spatie package tools and remove config file
    Ref #13, #15

--- src/GoogleAuthServiceProvider.php
+++ src/GoogleAuthServiceProvider.php
@@ -2,82 +2,19 @@
 
 namespace Glamstack\GoogleAuth;
 
-use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class GoogleAuthServiceProvider extends ServiceProvider
+class GoogleAuthServiceProvider extends PackageServiceProvider
 {
-    // use ServiceBindings;
-
-    public function boot() : void
+    public function configurePackage(Package $package): void
     {
-        $this->bootRoutes();
-    }
-
-    public function register()
-    {
-        $this->registerConfig();
-        $this->registerServices();
-    }
-
-    /**
-     * Register the package routes.
-     *
-     * @return void
-     */
-    protected function bootRoutes()
-    {
-        //$this->loadRoutesFrom(__DIR__.'/Routes/console.php');
-    }
-
-    protected function registerConfig() : void
-    {
-
-        //
-        // Merge config file into application config
-        //
-        // This allows users to override any module configuration values with
-        // their own values in the application config file.
-        //
-        $this->mergeConfigFrom(
-            __DIR__.'/Config/glamstack-google.php',
-            'glamstack-google'
-        );
-
-        if (! $this->app->runningInConsole()) {
-            return;
-        }
-
-        //
-        // Publish config file to application
-        //
-        // Once the `php artisan vendor::publish` command is run, you can use
-        // the configuration file values `$value = config('glamstack-gitlab.option');`
-        //
-        $this->publishes(
-            [
-                __DIR__.'/Config/glamstack-google.php' => config_path(
-                    'glamstack-google.php'
-                ),
-            ],
-            'glamstack-google'
-        );
-    }
-
-    /**
-     * Register package services in the container.
-     *
-     * @return void
-     */
-    protected function registerServices()
-    {
-        if (! property_exists($this, 'serviceBindings')) {
-            return;
-        }
-
-        foreach ($this->serviceBindings as $key => $value) {
-            is_numeric($key)
-                    ? $this->app->singleton($value)
-                    : $this->app->singleton($key, $value);
-        }
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('google-auth-sdk');
     }
 }
