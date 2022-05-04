90a737e11e41b2a38f440c797314e3048e266d84
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-04 14:47:48 -0400

    Add tests/TestCase.php
    Ref #4

---
+++ tests/TestCase.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Glamstack\GoogleAuth\Tests;
+
+use Glamstack\GoogleAuth\GoogleAuthServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TestCase extends Orchestra
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+    protected function getPackageProviders($app)
+    {
+        return [
+            GoogleAuthServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_apiclient_table.php.stub';
+        $migration->up();
+        */
+    }
+}
