8c560d729f93ea2aea83209fc73a3170f64dd217
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-05 06:55:56 -0400

    Update tests/Feature/AuthClientTest.php `api_scopes` to be a more generic scope

--- tests/Feature/AuthClientTest.php
+++ tests/Feature/AuthClientTest.php
@@ -6,7 +6,7 @@ use Glamstack\GoogleAuth\AuthClient;
 
 it('can authenticate with Google OAuth Server via json file', function(){
     $string_key_connection_config = [
-        'api_scopes' => ['https://www.googleapis.com/auth/ndev.clouddns.readwrite'],
+        'api_scopes' => ['https://www.googleapis.com/auth/cloud-platform.read-only'],
         'file_path' => 'tests/Storage/keys/integration_test_key.json'
     ];
     $client = new AuthClient($string_key_connection_config);
