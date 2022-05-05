1803c0cac2fb1be21a9a8ec643944eb4c91397fa
Dillon Wheeler <dwheeler@gitlab.com>
2022-05-05 10:17:34 -0400

    Update tests/Feature/AuthClientTest.php to add test for exceptions to be thrown

--- tests/Feature/AuthClientTest.php
+++ tests/Feature/AuthClientTest.php
@@ -13,3 +13,21 @@ it('can authenticate with Google OAuth Server via json file', function(){
     $auth_token = $client->authenticate();
     expect($auth_token)->toBeString();
 });
+
+it('will throw exception if authentication fails', function(){
+    $string_key_connection_config = [
+        'api_scopes' => ['https://www.googleapis.com/auth/cloud-platform.read-only'],
+        'file_path' => 'tests/Storage/keys/incorrect_key.json'
+    ];
+    $client = new AuthClient($string_key_connection_config);
+    $client->authenticate();
+})->expectExceptionMessage('Google SDK Authentication Error. Invalid JWT Signature.');
+
+it('will throw exception is permissions are incorrect', function(){
+    $string_key_connection_config = [
+        'api_scopes' => [],
+        'file_path' => 'tests/Storage/keys/integration_test_key.json'
+    ];
+    $client = new AuthClient($string_key_connection_config);
+    $auth_token = $client->authenticate();
+})->expectExceptionMessage('Invalid OAuth scope or ID token audience provided.');
